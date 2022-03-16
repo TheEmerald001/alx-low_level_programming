7e538880fbb3d9178aee6b9b6b5018df9a42c20a
Steve <austines811@gmail.com>
2022-03-16 14:27:06 -0700

    added sum of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * sum - Adds two integers.
+ * a: The first integer to be added.
+ * b: The second integer to be added.
+ *
+ * Return: The result of the addition.
+ */
+int main(void)
+{
+	int a, b, sum;
+	sum = a+b;
+	printf("%d\n", sum);
+	return (0);
+}
