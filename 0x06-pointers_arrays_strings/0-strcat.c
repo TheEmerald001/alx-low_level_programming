3ea3851bfa05ab2540d96d4f4f92c13cd8b0c263
Steve <austines811@gmail.com>
2022-03-24 04:12:45 -0700

    added function to contacanate two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,19 +11,16 @@
  */
 char *_strcat(char *dest, char *src)
 {
-  int i = 0, j;
-
-  while (dest[i])
+    int i = 0, j;
+    while (dest[i])
     {
-      i++;
+        i++;
     }
-  
-  for (j = 0; src[j] != 0; j++)
+    for (j = 0; src[j] != 0; j++)
     {
-      dest[i] = src[j];
-      i++;
+        dest[i] = src[j];
+        i++;
     }
-  
-  dest[i] = '\0';
-  return (dest);
+    dest[i] = '\0';
+    return (dest);
 }
